68b13cb0ff642a20be14408a738385825a39cada
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 20:44:50 -0300

    Update ex077.py

--- ex077.py
+++ ex077.py
@@ -1,4 +1,6 @@
-listaPalavras = ('VAMO', 'GALO', 'DISGRAÇA')
+"""Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais."""
+
+listaPalavras = ('Olá', 'tudo', 'bem?')
 for p in listaPalavras:
     print(f'\nNa palavra {p.upper()} temos ', end='')
     for letra in p:
