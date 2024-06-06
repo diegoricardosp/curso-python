839fd635a46ede58514fbac10be6cab6b4aec01d
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 20:43:19 -0300

    Update ex079.py

--- ex079.py
+++ ex079.py
@@ -1,3 +1,6 @@
+"""Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. 
+Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente."""
+
 listaNum = []
 while True:
     numDigi = int(input("Digite um valor: "))
