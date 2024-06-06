6f62dd6c410190ad9619879fd6e69831ef8e15e4
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 20:43:43 -0300

    Update ex078.py

--- ex078.py
+++ ex078.py
@@ -1,3 +1,5 @@
+"""Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista."""
+
 lista = []
 for v in range(0,5):
     lista.append(int(input(f'Digite um valor na posição {v}: ')))
