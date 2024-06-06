a3be65d0dfe37b4e8012bffc3d2cf9339f39bed8
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 20:42:54 -0300

    Update ex080.py

--- ex080.py
+++ ex080.py
@@ -1,3 +1,6 @@
+"""Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). 
+No final, mostre a lista ordenada na tela."""
+
 listaNum = []
 numNovo = 0
 for i in range(0,5):
