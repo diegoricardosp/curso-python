629c0b5ad4a4b3af0b176502bc5b6698d8ec6bd9
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 20:47:30 -0300

    Update ex073.py

--- ex073.py
+++ ex073.py
@@ -1,3 +1,10 @@
+"""Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. 
+Depois mostre: 
+a) Os 5 primeiros times. 
+b) Os últimos 4 colocados. 
+c) Times em ordem alfabética. 
+d) Em que posição está o time da Chapecoense."""
+
 tabela = ('Atlético - MG', 'Flamengo', 'Palmeiras', 'América - MG', 'Internacional', 'Grêmio', 'Athletico - PR', 'Santos', 'Corinthians', 'Bragantino', 'Bahia', 'Botafogo', 'Fluminense', 'Cuiabá', 'Fortaleza', 'Goiás', 'São Paulo', 'Vasco', 'Coritiba', 'Cruzeiro')
 
 bahia = tabela.index('Bahia')+1
