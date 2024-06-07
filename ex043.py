3f6ff5f1e127e5dd5e8e357da0c67079480dcba2
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 22:28:36 -0300

    Update ex043.py

--- ex043.py
+++ ex043.py
@@ -1,3 +1,9 @@
+"""Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo: 
+– IMC abaixo de 18,5: Abaixo do Peso 
+– Entre 18,5 e 25: Peso Ideal 
+– 25 até 30: Sobrepeso
+- de 30 até 40: Obesidade"""
+
 peso = float(input("Digite seu peso: "))
 alt = float(input("Digite sua altura: "))
 imc = peso / (alt * alt)
