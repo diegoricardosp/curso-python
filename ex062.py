465d23921636b908d8fd67acc5d67b43512cd581
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 21:44:06 -0300

    Update ex062.py

--- ex062.py
+++ ex062.py
@@ -1,3 +1,6 @@
+"""Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.""
+
+
 pt = int(input("Digite o primeiro valor: "))
 r = int(input("Digite a diferença entre os valores: "))
 termo = pt
