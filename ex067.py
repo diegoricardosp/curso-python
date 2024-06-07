4a7f28189b21a07385e1ef6eafee104797a99e78
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 21:21:48 -0300

    Update ex067.py

--- ex067.py
+++ ex067.py
@@ -1,3 +1,5 @@
+"""Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo."""
+                                                                                                                                                                       
 n = 0
 v = 1
 while True:
