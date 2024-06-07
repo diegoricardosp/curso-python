81d334e1b12d7b3deb64dd318e711d55f68fe0bc
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 22:13:17 -0300

    Update ex050.py

--- ex050.py
+++ ex050.py
@@ -1,3 +1,5 @@
+"""Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o."""
+
 r = 0
 for i in range(0, 6):
     n = int(input("Digite um valor: "))
