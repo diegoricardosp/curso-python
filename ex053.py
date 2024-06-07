74502e22a8d3626c161d739ea3ee98a233800438
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 22:12:09 -0300

    Update ex053.py

--- ex053.py
+++ ex053.py
@@ -1,4 +1,4 @@
-"""rie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços. 
+"""Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços. 
 Exemplos de palíndromos: 
 APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA."""
 
