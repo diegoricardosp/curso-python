f1c156f8cb5203163314e1a003e7477ea26a5a62
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 21:55:32 -0300

    Update ex059.py

--- ex059.py
+++ ex059.py
@@ -1,3 +1,7 @@
+"""Crie um programa que leia dois valores e mostre um menu na tela: 
+[ 1 ] somar [ 2 ] multiplicar [ 3 ] maior [ 4 ] novos números [ 5 ] sair do programa 
+Seu programa deverá realizar a operação solicitada em cada caso."""
+
 v1 = int(input("Digite um valor: "))
 v2 = int(input("Digite outro valor: "))
 opc = 0
