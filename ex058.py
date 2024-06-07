8fae24e54f4b3db503cbc2a457864349febd455a
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 21:56:00 -0300

    Update ex058.py

--- ex058.py
+++ ex058.py
@@ -1,3 +1,6 @@
+"""Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. 
+Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer."""
+
 from random import randrange
 cont = 1
 num = int(input("Tente adivinhar o número que pensei de 0 à 10: "))
