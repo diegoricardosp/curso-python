7614412f02c185966a5b8f83e75f9e40ae363b96
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 22:12:33 -0300

    Update ex052.py

--- ex052.py
+++ ex052.py
@@ -1,3 +1,4 @@
+"""Faça um programa que leia um número inteiro e diga se ele é ou não um número primo."""
 num = int(input("Digite um número inteiro: "))
 for i in range(2, num):
     if num % i == 0:
