33a3fc3d61b3d477e67bf2a0d35ec811913a9e8f
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 21:44:52 -0300

    Update ex061.py

--- ex061.py
+++ ex061.py
@@ -1,3 +1,5 @@
+"""Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while."""
+
 pt = int(input("Digite o primeiro valor: "))
 r = int(input("Digite a diferença entre os valores: "))
 termo = pt
