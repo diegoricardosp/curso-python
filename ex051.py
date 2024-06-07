31f74b87edf862d655d56a39aa43d42a5ea0ee31
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 22:12:56 -0300

    Update ex051.py

--- ex051.py
+++ ex051.py
@@ -1,3 +1,5 @@
+""""Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão."""
+
 pt = int(input("Digite o primeiro valor: "))
 r = int(input("Digite a diferença entre os valores: "))
 décimo = pt + (10 - 1) * r
