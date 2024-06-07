b13a7083af40a7d61fe53426289746c4037001e4
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 22:27:11 -0300

    Update ex044.py

--- ex044.py
+++ ex044.py
@@ -1,3 +1,9 @@
+"""labore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento: 
+– à vista dinheiro/cheque: 10% de desconto 
+– à vista no cartão: 5% de desconto 
+– em até 2x no cartão: preço formal
+- 3x ou mais no cartão: 20%"""
+
 prod = float(input("Qual o valor do produto? R$"))
 cond = int(input("Qual a forma de pagamento?\n [1] À vista\n [2] Á vista no cartão\n [3] Em até 2x no cartão\n [4] Em 3x ou mais no cartão\n"))
 if cond == 1:
