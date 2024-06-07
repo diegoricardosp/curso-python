32be93e6f7bb78ad2ed97dfc70d26435cfd73615
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 22:13:42 -0300

    Update ex049.py

--- ex049.py
+++ ex049.py
@@ -1,3 +1,5 @@
+"""Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for."""
+
 tab = int(input("Digite o valor que deseja saber a tabuada: "))
 for i in range(1, 11):
     result = tab * i
