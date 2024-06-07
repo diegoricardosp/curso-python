be69397ca1bb893fbe72ac2825b98b63e354e6ea
diegoricardosp <135927427+diegoricardosp@users.noreply.github.com>
2024-06-06 22:10:55 -0300

    Update ex055.py

--- ex055.py
+++ ex055.py
@@ -1,4 +1,6 @@
-'''maior = 0
+'''Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+maior = 0
 menor = 0
 for i in range(1, 6):
     peso = float(input("Digite o {}° peso: ".format(i)))
